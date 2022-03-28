41d5cb4ebe00a84edfb3d4a3c8959c7a4f1bc8ca
Fangyi Zhou <me@fangyi.io>
2022-03-28 23:39:36 +0100

    Minor refactor

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,21 @@ use std::fs;
 use std::io;
 use std::path::Path;
 
+fn discover_git_repos(dir: &Path) -> Result<Vec<git2::Repository>, io::Error> {
+    let mut repos = Vec::new();
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let inner_path = entry.path();
+        if inner_path.is_dir() {
+            if let Ok(repo) = git2::Repository::open(inner_path) {
+                //println!("Found git repo at {:?}", repo.path())
+                repos.push(repo)
+            }
+        }
+    }
+    Ok(repos)
+}
+
 fn main() -> Result<(), io::Error> {
     let dir = match env::args().nth(1) {
         Some(dir) => dir,
@@ -13,14 +28,9 @@ fn main() -> Result<(), io::Error> {
     if !path.is_dir() {
         panic!("Input {:?} is not a directory", path);
     }
-    for entry in fs::read_dir(dir)? {
-        let entry = entry?;
-        let inner_path = entry.path();
-        if inner_path.is_dir() {
-            if let Ok(repo) = git2::Repository::open(inner_path) {
-                println!("Found git repo at {:?}", repo.path())
-            }
-        }
+    let repos = discover_git_repos(path)?;
+    for repo in repos {
+        println!("Found git repo at {:?}", repo.path())
     }
     Ok(())
 }
