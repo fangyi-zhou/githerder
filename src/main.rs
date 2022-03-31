db0d38e97dc8c36f37edf76e4bf2281ba0ce2372
Fangyi Zhou <me@fangyi.io>
2022-04-01 00:29:57 +0100

    Find workdirs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 extern crate git2;
+use git2::{Repository, RepositoryState};
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::io;
 use std::path::Path;
 
-fn discover_git_repos(dir: &Path) -> Result<Vec<git2::Repository>, io::Error> {
+fn discover_git_repos(dir: &Path) -> Result<Vec<Repository>, io::Error> {
     let mut repos = Vec::new();
     for entry in fs::read_dir(dir)? {
         let entry = entry?;
         let inner_path = entry.path();
         if inner_path.is_dir() {
-            if let Ok(repo) = git2::Repository::open(inner_path) {
+            if let Ok(repo) = Repository::open(inner_path) {
                 //println!("Found git repo at {:?}", repo.path())
                 repos.push(repo)
             }
@@ -19,26 +21,29 @@ fn discover_git_repos(dir: &Path) -> Result<Vec<git2::Repository>, io::Error> {
     Ok(repos)
 }
 
-fn process_repo(repo: git2::Repository) -> Result<(), io::Error> {
-    if let git2::RepositoryState::Clean = repo.state() {
+fn get_workdir_for_clean_repos(repo: &Repository) -> Result<Option<&Path>, Box<dyn Error>> {
+    if let RepositoryState::Clean = repo.state() {
         if let Ok(statuses) = repo.statuses(None) {
             if statuses.iter().all(|status_entry| {
                 status_entry.status().is_empty() || status_entry.status().is_ignored()
             }) {
-                println!("Clean git repo at {:?}", repo.path());
+                // println!("Clean git repo at {:?}", repo.path());
+                Ok(repo.workdir())
             } else {
-                println!("Unclean git repo at {:?}", repo.path());
+                println!("Skipping unclean git repo at {:?}", repo.path());
+                Ok(None)
             }
+        } else {
+            Ok(None)
         }
-        Ok(())
     } else {
         // Ignore repos that are not clean
-        println!("Unclean git repo at {:?}", repo.path());
-        Ok(())
+        println!("Skippuing unclean git repo at {:?}", repo.path());
+        Ok(None)
     }
 }
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<(), Box<dyn Error>> {
     let dir = match env::args().nth(1) {
         Some(dir) => dir,
         None => String::from("."),
@@ -48,8 +53,12 @@ fn main() -> Result<(), io::Error> {
         panic!("Input {:?} is not a directory", path);
     }
     let repos = discover_git_repos(path)?;
-    for repo in repos {
-        process_repo(repo)?;
+    let workdirs: Vec<&Path> = repos
+        .iter()
+        .filter_map(|repo| get_workdir_for_clean_repos(repo).ok().flatten())
+        .collect();
+    for workdir in workdirs.iter() {
+        println!("Workdir to pull: {}", workdir.display());
     }
     Ok(())
 }
